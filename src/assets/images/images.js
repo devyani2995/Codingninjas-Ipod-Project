818821202960dc03b538dcf3204aa256712218b5
devyani2995 <devyanisingh42@gmail.com>
2024-07-03 13:45:03 +0530

    first commit

---
+++ src/assets/images/images.js
@@ -0,0 +1,31 @@
+import wallpaper1 from "./wallpaper1.jpeg";
+import wallpaper2 from "./wallpaper2.jpeg";
+import wallpaper3 from "./wallpaper3.jpg";
+import wallpaper4 from "./wallpaper4.jpg";
+import wallpaper5 from "./wallpaper5.jpg";
+import coverflow from "./coverflowImgage.jpg";
+import games from "./i1.jpg";
+import allsongs from "./allsongs.gif";
+import artists from "./artistsImg.jpg";
+import albums from "./albumImg.jpg";
+import song1Img from "./i2.png";
+import song2Img from "./i3.jpg";
+import song3Img from "./i4.jpg";
+
+const images = {
+	wallpaper1,
+	wallpaper2,
+	wallpaper3,
+	wallpaper4,
+	wallpaper5,
+	coverflow,
+	games,
+	allsongs,
+	artists,
+	albums,
+	song1Img,
+	song2Img,
+	song3Img,
+};
+
+export default images;
---
+++ src/assets/songs/songs.js
@@ -0,0 +1,11 @@
+import music1 from "./s1.mp3";
+import music2 from "./s2.mp3";
+import music3 from "./s3.mp3";
+
+const songs = {
+	music1,
+	music2,
+	music3,
+};
+
+export default songs;
